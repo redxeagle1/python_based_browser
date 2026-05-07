59ac969d71cbbf40a419ca0d1f11fda95290bfac
Saad Salama <redxeagle97@gmail.com>
2026-05-07 11:10:54 +0300

    finish the request function that will:     - make a socket     - establish a connection via that socket     - then the socket send http request in the form of     	```http 	GET {self.path} HTTP/1.0\r\n 	Host: {self.host}\r\n 	\r\n # inserts a balnk line to inform the end of the request 	```     - split the response into headers and the content of the request NEXT: finalizing the implementation of the show and load mechanism to showcase the message response

--- browser_utils.py
+++ browser_utils.py
@@ -24,4 +24,54 @@ class URL:
         # Getting the path
         self.path = "/" + url
 
-    def request(self):
+    def request(self):  # Telnet IN PYTHON
+        """A socket has
+        - address family, which tells you how to find the other computer.
+        - type, which describes the sort of conversation that’s going to happen
+        - protocol, which describes the steps by which the two computers
+        will establish a connection.
+        """
+        s = socket.socket(
+            family=socket.AF_INET,  # Address families have names that begin with `AF`
+            type=socket.SOCK_STREAM,  # Types have names that begin with `SOCK`.
+            proto=socket.IPPROTO_TCP,  # Protocols have names that depend on the address family.
+        )
+        s.connect((self.host, 80))
+        # `connect` takes a single argument, and that argument is a pair of a host and a port.
+        # This is because different address families have different numbers of arguments.
+
+        # request formating
+        request = f"GET {self.path} HTTP/1.0\r\n"
+        request += f"Host: {self.host}\r\n"
+        request += "\r\n"
+
+        # sending the request
+        s.send(request.encode("utf8"))
+        # getting the respone
+        response = s.makefile("r", encoding="utf8", newline="\r\n")
+
+        # split the response into pieces
+        statusline = response.readline()
+        version, status, epxplanation = statusline.split(" ", 2)
+
+        # split each line at the first colon then
+        # fill in a map of header names to header values.
+        # NOTE:Headers are case-insensitive, so normalize them to lower case
+        response_headers = {}
+        while True:
+            line = response.readline()
+            if line == "\r\n":
+                break
+            header, value = line.split(":", 1)
+            response_headers[header.casefold()] = value.strip()
+
+        # Headers can describe all sorts of information,
+        # couple of headers are especially important because they tell us that the data
+        assert "transfer-encoding" not in response_headers
+        assert "content-encoding" not in response_headers
+        content = response.read()
+        s.close()
+
+        return content
+
+    def show(body): ...
